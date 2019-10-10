18fc42729390272a4d8e631a8ef4d3536242e465
Alex Yi <probesalesman@gmail.com>
2019-10-09 22:32:21 -0400

    adds routers to express server to handle users and dogs

--- server/app.js
+++ server/app.js
@@ -1,44 +1,16 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const client = require("./db");
+const path = require("path");
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get("/", async (req, res, next) => {
-  try {
-    const query = "select users.* from users";
-    const data = await client.query(query);
-    res.json(data.rows);
-  } catch(err) {
-    next(err);
-  }
-});
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/:id", async (req, res, next) => {
-  try {
-    const query = "select users.* from users where users.id = $1";
-    const data = await client.query(query, [req.params.id]);
+app.use("/api", require("./api"));
 
-    if (!data.rows.length) {
-      const new404 = new Error("Page not found");
-      new404.status = 404;
-      throw new404;
-    }
-
-    res.json(data.rows[0]);
-  } catch(err) {
-    next(err);
-  }
-});
-
-app.post("/", (req, res, next) => {
-  console.log("req.body is alive", req.body);
-
-  res.sendStatus(201);
-});
 
 app.use((err, req, res, next) => {
   if (err.status === 404) {
